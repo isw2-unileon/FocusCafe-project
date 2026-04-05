5ff0a251352306c53d8ae54aedc53d46babb185e
Daniela-ule <dalijg00@estudiantes.unileon.es>
2026-04-05 12:54:42 +0200

    chore: - modified Home with new components - fix error in user_service

--- frontend/src/services/user_service.ts
+++ frontend/src/services/user_service.ts
@@ -1,15 +1,9 @@
 import { UserStats } from "@/types/user";
 
 export const getRemoteUserStats = async (userId: number): Promise<UserStats> => {
+    const response = await fetch(`/api/user/${userId}/stats`);
     if(!response.ok){
         throw new Error("Failed to fetch user stats");
     }
     return response.json();
-    /*return {
-        id: userId,
-        name: "Pepe",
-        energy: 150,
-        max_energy: 500,
-        level: 3
-    };*/
 }
