b639124412a203b5c6e759a75504bd04e8959d55
Daniela-ule <dalijg00@estudiantes.unileon.es>
2026-04-19 22:47:17 +0200

    chore: fix error in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           {/*Public routes*/}
+          <Route path="/" element={<Navigate to="/login" replace />} /> 
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
