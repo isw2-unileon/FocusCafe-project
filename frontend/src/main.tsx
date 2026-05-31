a370683edc879e8939e9092fd3a5802d8322e9c6
Daniela-ule <dalijg00@estudiantes.unileon.es>
2026-05-31 14:02:17 +0200

    chore: fix bug in ws and translate spanish to english

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 
 
-localStorage.setItem('userId', '123'); // Simulate logged-in user with ID 123
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
